e7eda54b01c14ecb3e2a78c3b33d72f2d803bfca
paulbbrouillette <paulbbrouillette@gmail.com>
2025-09-02 22:14:29 -0400

    Style change

--- src/components/Staff.js
+++ src/components/Staff.js
@@ -1,6 +1,5 @@
 import '../css/HeaderLinksCSS.css';
-import PaulPic from '../assets/OtherPics/Paul.JPG';
-import Leprechaun from '../assets/OtherPics/Leprechaun.jpg'
+import PaulPic from '../assets/OtherPics/Paul.png';
 import LinkedInImage from '../assets/OtherPics/LinkedInLogo.png';
 import GitHubImage from '../assets/OtherPics/GitHubLogo.png';
 import logo from '../assets/OtherPics/logoblue.png';
@@ -20,7 +19,7 @@ export default function About() {
             <h1>Staff</h1>
             <div class="SingleStaff">
                 <div class="NameImage">
-                    <h2>Paul Brouillette</h2>
+                    <h2>Paul</h2>
                     <img src={PaulPic} alt="Paul Brouillette"></img>
                 </div>
                 <div class="RoleBio">
@@ -35,21 +34,6 @@ export default function About() {
                         and it would be easier to go outside during a thunderstorm and try to dodge rain than it would be to avoid potholes on the roads; nonetheless, it's his home for at least the time being.
                         He works as a programmer during the day but still finds time to serve the community in his free time by writing quality CRAP articles.
                         While he may not like his job, he can't find another one, and he's too poor to afford a house, he at least has this going for him: 
-                    </p>
-                </div>
-            </div>
-            <div class="SingleStaff">
-                <div class="NameImage">
-                    <h2>Ó Maolmhuaidh</h2>
-                    <img src={Leprechaun} alt="Ó Maolmhuaidh"></img>
-                </div>
-                <div class="RoleBio">
-                    <h3>Social Media Manager &#9679; Journalist</h3>
-                    <p>
-                        Hailing from County Sligo, Michael Ó Maolmhuaidh's ancestors came to America seeking new oppurtunities and a better life, what they found was Albany, NY.
-                        Ó Maolmhuaidh has worked as a journalist in the past putting out crap articles, but now he pushes out CRAP articles. Our social media accounts 
-                        are managed by him, so be nice or he'll start crying. When he does write articles, you're in for a treat because when Ó Maolmhuaidh speaks you shut your ass up and listen. He likes Albany
-                        because the miserable weather reminds him of his ancestrial homeland, but he often wonders why his relatives didn't immigrate to Boston instead.
                     </p>
                 </div>
             </div>
